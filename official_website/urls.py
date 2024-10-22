3bdfcfbf2c00a1a725f8540dda70f963bb9acc41
MuliJob <munyoki912@gmail.com>
2024-10-22 15:55:12 +0300

    added service details page

--- official_website/urls.py
+++ official_website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('services-details', views.service_detail, name='service_detail')
 ]
